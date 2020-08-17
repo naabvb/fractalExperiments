f8a76d0d2c521e7d5ca4bc61ee654bd96d79e77e
naabvb <46524999+naabvb@users.noreply.github.com>
2020-08-17 19:52:57 +0300

    Clean up and refactoring

--- sketch.js
+++ sketch.js
@@ -1,236 +1,132 @@
 var canvas;
 var loadingIcon;
-var size;
-var trees = [];
-var created = [];
+var forestSize;
+var treeRoots = [];
+var createdTrees = [];
 var leafs = [];
+var landscapeCoordinates = [];
 
-var blue = '#325876';
-//var blue = color(50, 88, 118);
-var dx = 200;
-var dy = 50;
+var field;
+var scale;
+var bounds;
 
-let field;
-
-let scale,
-    theme,
-    bounds;
-var start;
-
-var done = false
-var remap = false
+var setupDone = false
+var startPerlin = false
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
-    //drawGradient();
     canvas.hide();
-    trees = [];
-    start = true;
+
+    // Set background gradient based on presets in background.js
     setGradient(20)
-    //noStroke();
-    // drawGradient();
+
     loadingIcon = document.getElementById('loaderIcon')
-    if (windowWidth < 100) {
-        size = 2
-    }
-    else {
-        size = random(4, 7);
-    }
-    //  size = random(2, 5);
-    // noLoop();
+
+    // Probably unnecessary scaling
+    if (windowWidth < 100) forestSize = 2;
+    else forestSize = random(4, 7);
+
     let root;
-    for (let i = 0; i < size; i++) {
-        //if (windowWidth < 200) {
-        //    root = createVector(random(0, windowWidth), random(height + 10, height + 30));
-        //}
-        //else {
-        //    root = createVector(random(0, windowWidth), random(height + 70, height + 100));
-        //}
+
+    // Calculate tree root coordinates
+    for (let i = 0; i < forestSize; i++) {
         root = createVector(random(0, windowWidth), random(height - 90, height))
-        // let root = createVector(random(0, windowWidth), random(height + 70, height + 100));
-        let sz = random(8, 12)
-        trees.push({ 'root': root, 'sz': sz });
+        treeRoots.push({ 'root': root, 'sz': random(8, 12) });
     }
-    for (let i = 0; i < trees.length; i++) {
-        created.push(new Branch(trees[i]['sz'], trees[i]['root'], 0, i));
+    for (let i = 0; i < treeRoots.length; i++) {
+        createdTrees.push(new Branch(treeRoots[i]['sz'], treeRoots[i]['root'], 0, i));
     }
-    //console.log(leafs)
 
-
-    // for (let i = 0; i < trees.length; i++) {
-    //     drawBranch(random(7, 12), trees[i], 0);
-    // }
     scale = random(8e2, 2e3);
-    // theme = {name: "Night Sky", 
-    //				// colors: ["#45217C", "#0799F2", "#FFFFFF"], 
-    //				 colors: getColor(),}
-    //                // background: color(8,3,20)}; //"#150832"
-    //console.log(theme.colors);
+
     field = new ParticleSystem()
     field.colors = getColor();
     field.build(2000);
-    drawClouds();
-    //canvas.show();
-    //setGradient(6)
+    createLandscape();
 }
 
 function draw() {
-    // setGradient(6)
-    // canvas.show();
     noStroke();
-    //field.render();
-    if (!remap && !done) {
+
+    // Stop stars after two iterations to create stars
+    if (!startPerlin && !setupDone) {
         field.render();
         field.render();
-
     }
 
-    if (remap) {
-        field.render();
-    }
-    //drawrect()
-    //drawClouds();
-    drawmounts();
-    drawrect()
-    // debugger;
-    for (let i = 0; i < trees.length; i++) {
-        //console.log(trees[i]['root'].x)
-        //console.log(trees.length)
-        // fill(0,64,128,48);
-        //  ellipseMode(CENTER);
-        // console.log(coords[i])
-        // ellipse(coords[i].x, coords[i].y, 30, 8);
-        //fill(0,64,128,48);
-        drawTree(i);
-        //fill(0,64,128,48);
-        //console.log('drwan')
-    }
+    // If mouse clicked, start perlin noise
+    if (startPerlin) field.render();
 
-    for (let i = 0; i< leafs.length; i++) {
-        leafs[i].show()
-    }
+    drawLandscape();
+    drawGround()
+    drawTrees();
+    drawLeafs();
 
-    //drawClouds();
-
-    if (!done) {
-        done = true;
+    // Clear out loading icons and show canvas
+    if (!setupDone) {
+        setupDone = true;
         loadingIcon.style.visibility = 'hidden';
         canvas.show();
     }
-
-
     noStroke();
-    //done = true
-
-    //  if (trees.length > 0 && start) {
-    //      noStroke();
-    //      field.render();
-    //      field.render();
-    //      field.render();
-    //      debugger;
-    //      for (let i = 0; i < trees.length; i++) {
-    //          showem();
-    //      }
-    //      //trees = []
-    //      loadingIcon.style.visibility = 'hidden';
-    //     // noStroke();
-    //     // field.render();
-    //     // field.render();
-    //     // field.render();
-    //      canvas.show();
-    //      start = false;
-    //  //   // field.render();
-    //  }
-    //debugger;
-
-    //canvas.show();
-    // field.render();
-    //drawShit();
-    //noStroke();
-    // background();
-    // field.render();
-
 }
 
-var vertexAR = []
-
-function drawClouds() {
-    let j = 0
-    //for (let j = 0; j < height/3; j++) {
-    
-        // montains
-        //beginShape();   
-       // fill(lerpColor(color(43, 29, 14), color(124, 144, 63), j/height));
-        //fill(color(24, 16, 8))
-       // vertex(0, height);
-        vertexAR.push({'x':0, 'y':height})
-        for (let i = 0; i <= width; i +=2) {
-          n = noise(i/dx, j/dy);
-          n = map(n, 0, 1, 0, height/2 - j);
-          vertexAR.push({'x':i, 'y':height-n})
-          //vertex(i, height - n);
-          //console.log('asd')
-        }
-        vertexAR.push({'x':width, 'y':height})
-        //vertex(width, height);
-        //endShape();
-        //break;
-    //}    
+// Draws all leafs
+function drawLeafs() {
+    for (let i = 0; i < leafs.length; i++) {
+        leafs[i].show();
+    }
 }
 
-function drawmounts() {
+// Creates vertex coordinates for the landscape
+function createLandscape() {
+    let dx = 200;
+    let dy = 50;
+    let j = 0;
+    landscapeCoordinates.push({ 'x': 0, 'y': height })
+    for (let i = 0; i <= width; i += 2) {
+        n = noise(i / dx, j / dy);
+        n = map(n, 0, 1, 0, height / 2 - j);
+        landscapeCoordinates.push({ 'x': i, 'y': height - n })
+    }
+    landscapeCoordinates.push({ 'x': width, 'y': height })
+}
+
+// Draw landscape using the previously generated coordinates
+function drawLandscape() {
     beginShape();
     fill(color(24, 16, 8))
-    for (let i = 0; i < vertexAR.length; i++) {
-        vertex(vertexAR[i]['x'], vertexAR[i]['y'])
+    for (let i = 0; i < landscapeCoordinates.length; i++) {
+        vertex(landscapeCoordinates[i]['x'], landscapeCoordinates[i]['y'])
     }
     endShape();
 }
 
-
-function drawClouds2() {
-    let j = 0
-    //for (let j = 0; j < height/3; j++) {
-    
-        // montains
-        beginShape();   
-       // fill(lerpColor(color(43, 29, 14), color(124, 144, 63), j/height));
-        fill(color(24, 16, 8))
-        vertex(0, height);
-        for (let i = 0; i <= width; i +=2) {
-          n = noise(i/dx, j/dy);
-          n = map(n, 0, 1, 0, height/2 - j);
-          vertex(i, height - n);
-          console.log('asd')
-        }
-        vertex(width, height);
-        endShape();
-        //break;
-    //}    
-}
-
-function drawrect() {
+// Draw foreground
+function drawGround() {
     fill(color(28, 19, 8))
     rect(0, height - 100, width, height / 10);
     fill(color(28, 19, 8))
 }
 
-
-function drawTree(a) {
-    for (let i = 0; i < created.length; i++) {
-        if (created[i].treenum == a) {
-            created[i].show()
-        }
-        //console.log(created[0])
+// Draws the trees previously generated
+function drawTrees() {
+    for (let i = 0; i < treeRoots.length; i++) {
+        drawTree(i);
     }
 }
 
+// Draws a tree and all it's branches
+function drawTree(a) {
+    for (let i = 0; i < createdTrees.length; i++) {
+        if (createdTrees[i].treenum == a) {
+            createdTrees[i].show()
+        }
+    }
+}
 
+// On mouse click, start perlin noise
 function mouseClicked() {
-    //loadingIcon.style.visibility = 'visible';
-    // console.log(items2)
-    //setup();
-    remap = true;
+    startPerlin = true;
     redraw();
-
 }
--- trees.js
+++ trees.js
@@ -1,3 +1,6 @@
+// Branch generator class. Lowering girth requirements for next branch will result in more detail
+// but with longer loading times.
+
 class Branch {
     constructor(girth, start, direction, treenum) {
         this.girth = girth;
@@ -10,65 +13,51 @@ class Branch {
         this.treenum = treenum;
 
         this.show = function () {
-           // console.log('drawn')
             strokeWeight(this.girth);
-            stroke(this.girth -1);
+            stroke(this.girth - 1);
             line(this.startx, this.starty, this.endx, this.endy);
-            //if (done) {
-            //    console.log(this.girth);
-            //    console.log(this.startx)
-            //    console.log(this.starty)
-            //    console.log(this.endx)
-            //    console.log(this.endy)
-            //}
         };
 
-        for(let i = 0; i < random(2,6); i++) {
-            let end = createVector(start.x +sin(direction)*girth*2.5, start.y -cos(direction)*girth*2.5);
-           // stroke(girth+5);
-           // strokeWeight(girth);  
-           // line(start.x, start.y, end.x, end.y);
+        for (let i = 0; i < random(2, 6); i++) {
+            let end = createVector(start.x + sin(direction) * girth * 2.5, start.y - cos(direction) * girth * 2.5);
             this.endx = end.x;
             this.endy = end.y
             start.x = end.x;
             start.y = end.y;
-            direction += random(0.3)-random(0.3);
+            direction += random(0.3) - random(0.3);
         }
 
-        if(girth > 0.6) {
-            let branchA = random(0.5,0.99);
-            let branchB = 1 -branchA;
-            let sign = (int(random(2))*2-1); // -1 or +1
-            let angle = random(0.4,1.2) *sign; // angle between branchA and branchB
-            let directionA = direction *0.9 +angle*branchB;
-            let directionB = direction *0.9 -angle*branchA;
-            created.push(new Branch(girth *sqrt(branchA), createVector(start.x, start.y), directionA, this.treenum));
-            created.push(new Branch(girth *sqrt(branchB), createVector(start.x, start.y), directionB, this.treenum));
-            if(girth < 1.2 && random(1) < 0.1) {
+        if (girth > 0.6) {
+            let branchA = random(0.5, 0.99);
+            let branchB = 1 - branchA;
+            let sign = (int(random(2)) * 2 - 1);
+            let angle = random(0.4, 1.2) * sign;
+            let directionA = direction * 0.9 + angle * branchB;
+            let directionB = direction * 0.9 - angle * branchA;
+            createdTrees.push(new Branch(girth * sqrt(branchA), createVector(start.x, start.y), directionA, this.treenum));
+            createdTrees.push(new Branch(girth * sqrt(branchB), createVector(start.x, start.y), directionB, this.treenum));
+            if (girth < 1.2 && random(1) < 0.1) {
                 leafs.push(new Leaf(start));
             }
         }
     }
 }
 
+// Class for generating and showing the leafs. Currently splits between two colors
 class Leaf {
     constructor(position) {
         this.position = position
-        this.val = random(1,10)
+        this.val = random(1, 10)
 
         this.show = function () {
             noStroke()
             if (this.val > 5) {
-                fill(255,250,240);
+                fill(255, 250, 240);
             }
             else {
                 fill(255, 183, 197);
             }
-           // let sz = 4
-	        ellipse(this.position.x, this.position.y+6, 2,4);
-
-         };
+            ellipse(this.position.x, this.position.y + 6, 2, 4);
+        };
     }
 }
-
-
