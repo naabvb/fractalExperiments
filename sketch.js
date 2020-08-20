e773d81e05749a82991758a34460d4d667dc43fb
naabvb <46524999+naabvb@users.noreply.github.com>
2020-08-20 16:48:09 +0300

    Add mountain tops

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var trunkCoordinates = [];
 var createdTrees = [];
 var leafs = [];
 var landscapeCoordinates = [];
+var mountainTopCoordinates = [];
 
 var field;
 var scale;
@@ -63,6 +64,7 @@ function draw() {
     // If mouse clicked, start perlin noise
     if (startPerlin) field.render();
     drawLandscape();
+    drawMountainTops();
     drawGround();
     drawTrees();
     drawLeafs();
@@ -95,6 +97,7 @@ function createLandscape() {
         landscapeCoordinates.push({ 'x': i, 'y': height - n });
     }
     landscapeCoordinates.push({ 'x': width, 'y': height });
+    getMountainTops();
 }
 
 // Draw landscape using the previously generated coordinates
@@ -105,6 +108,112 @@ function drawLandscape() {
         vertex(landscapeCoordinates[i]['x'], landscapeCoordinates[i]['y']);
     }
     endShape();
+}
+
+// Overly complicated function to get safe areas for mountain tops without drawing over nothing
+function getMountainTops() {
+    let minmax = getMinMax();
+    let tresh = (minmax.max - minmax.min) * 0.10 + minmax.min;
+    let areas = [];
+    let index1 = 0;
+    let index2 = 0;
+    for (let i = 0; i < landscapeCoordinates.length; i++) {
+        if (landscapeCoordinates[i].y < tresh) {
+            index1 = i;
+            let previous = landscapeCoordinates[i].y;
+            let highest = Number.NEGATIVE_INFINITY;
+            let highest_i = 0;
+            for (let j = i; j < landscapeCoordinates.length; j++) {
+                if (landscapeCoordinates[j].y > previous) {
+                    if (landscapeCoordinates[j].y > highest) {
+                        highest = landscapeCoordinates[j].y;
+                        highest_i = j;
+                    }
+                }
+                if (landscapeCoordinates[j].y > landscapeCoordinates[index1].y) {
+                    index2 = j;
+                    i = j + 1;
+                    if (landscapeCoordinates[index2].x < width - 3) {
+                        areas.push({ 's': index1, 'e': index2 });
+                    }
+                    index1 = 0;
+                    index2 = 0;
+                    break;
+                }
+                if (landscapeCoordinates[index1].y < highest) {
+                    index2 = highest_i;
+                    i = highest_i + 1;
+                    if (landscapeCoordinates[index2].x < width - 3) {
+                        areas.push({ 's': index1, 'e': index2 });
+                    }
+                    index1 = 0;
+                    index2 = 0;
+                    break;
+                }
+                previous = landscapeCoordinates[j].y;
+            }
+        }
+    }
+
+    for (let i = 0; i < areas.length; i++) {
+        let raw_coords = landscapeCoordinates.slice(areas[i].s, areas[i].e);
+        if (raw_coords.length > 1) {
+            let done_coords = addNoise(raw_coords);
+            mountainTopCoordinates.push(done_coords);
+        }
+    }
+}
+
+// Adds some random noise to mountain tops, could definitely be improved
+function addNoise(data) {
+    let first = data[0];
+    let last = data[data.length - 1];
+    let middle = Math.round((first.x + last.x) / 2);
+    let middle1 = Math.round((last.x + middle) / 2);
+    let middle105 = Math.round((middle1 + last.x) / 2);
+    let middle115 = Math.round((middle + middle1) / 2);
+    let middle2 = Math.round((first.x + middle) / 2);
+    let middle205 = Math.round((middle2 + middle) / 2);
+    let middle225 = Math.round((first.x + middle2) / 2);
+
+    data.push({ 'x': middle105, 'y': random(first.y + 10, first.y + 18 + data.length / 1.5) });
+    data.push({ 'x': middle1, 'y': random(first.y + 10, first.y + 18 + data.length / 1.5) });
+    data.push({ 'x': middle115, 'y': random(first.y + 10, first.y + 18 + data.length / 1.5) });
+    data.push({ 'x': middle, 'y': random(first.y + 10, first.y + 18 + data.length / 1.5) });
+    data.push({ 'x': middle205, 'y': random(first.y + 10, first.y + 18 + data.length / 1.5) });
+    data.push({ 'x': middle2, 'y': random(first.y + 10, first.y + 18 + data.length / 1.5) });
+    data.push({ 'x': middle225, 'y': random(first.y + 10, first.y + 18 + data.length / 1.5) });
+
+    return data;
+}
+
+function drawMountainTops() {
+    for (let i = 0; i < mountainTopCoordinates.length; i++) {
+        drawMountainTop(mountainTopCoordinates[i]);
+    }
+}
+
+// Draws a mountain top
+function drawMountainTop(tiedot) {
+    beginShape();
+    fill(110);
+    noStroke();
+    for (let i = 0; i < tiedot.length; i++) {
+        vertex(tiedot[i]['x'], tiedot[i]['y']);
+    }
+    endShape();
+}
+
+function getMinMax() {
+    let lowest = Number.POSITIVE_INFINITY;
+    let highest = Number.NEGATIVE_INFINITY;
+    let tmp;
+    for (let i = landscapeCoordinates.length - 1; i >= 0; i--) {
+        tmp = landscapeCoordinates[i].y;
+        if (tmp < lowest) lowest = tmp;
+        if (tmp > highest) highest = tmp;
+    }
+    return { 'min': lowest, 'max': highest };
 }
 
 // Draw some clouds
@@ -121,6 +230,7 @@ function drawClouds() {
 
 // Draw foreground
 function drawGround() {
+    noStroke();
     rectMode(CORNER);
     fill(color(28, 19, 8));
     rect(0, height * 0.90, width, height * 0.85);
